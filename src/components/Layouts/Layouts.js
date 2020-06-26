954a2b47466292eb767e7cab7dd172c2deacf9b7
Sergiu Corb <sergiucorb@Sergius-MacBook-Air.local>
2020-06-26 19:19:22 +0300

    sidedraw

--- src/components/Layouts/Layouts.js
+++ src/components/Layouts/Layouts.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layouts.css';
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layouts = (props) => (
-    <Aux>
-        <Toolbar/>
-        <main className={classes.Container}>
-            {props.children}
-        </main>
-    </Aux>
-);
-export default layouts;
+class Layouts extends Component {
+
+    state = {
+        showSideDrawer: false
+    };
+
+    clickMenuHandler = () => {
+        this.setState({showSideDrawer: true});
+    };
+
+    closeSideDrawer = () => {
+        this.setState({showSideDrawer: false});
+    };
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar clicked={this.clickMenuHandler}/>
+                <SideDrawer clicked={this.closeSideDrawer} show={this.state.showSideDrawer}/>
+                <main className={classes.Container}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layouts;
